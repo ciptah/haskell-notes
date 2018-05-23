0ef030959f1e46876d8ae2118e3e0d200f0d1266
cipta <ciphwn@gmail.com>
2018-05-23 06:13:19 -0400

    encode PMFs

--- RandomVariable.hs
+++ RandomVariable.hs
@@ -3,7 +3,9 @@
 
 module RandomVariable (
   RandomVariable,
-  isRandomVariable
+  invert,
+  isRandomVariable,
+  randomVariable
 ) where
 
 import Sets
@@ -36,3 +38,33 @@ isRandomVariable pspace rv =
   where borelSubsets = asSet $ BorelReals :: Collection (Set Double)
         originalF = asSet $ events pspace
         inverseOf interval = invert (outcomes pspace) rv interval
+
+randomVariable :: ProbabilitySpace a -> (a -> Double) -> RandomVariable a
+randomVariable ps rv
+    | isRandomVariable ps rv = RandomVariable ps rv
+    | otherwise = error "Invalid RV and PSpace combo"
+
+-- Probability mass/density functions
+-- These can be defined according to the space + random variable, or
+-- arbitrarily from an unknown space
+
+-- We still retain knowledge of the original outcome types, even if they're
+-- not important
+data PMF a = StrictPMF (RandomVariable a) | ArbitraryPMF (Double -> Double)
+
+getPMFFormalRV :: PMF a -> RandomVariable a
+getPMFFormalRV (StrictPMF x) = x
+getPMFFormalRV _ = error "Unavailable"
+
+-- (Wikipedia) "a measurable space whose underlying σ-algebra is discrete..."
+isDiscrete :: SigmaAlgebra a -> Bool
+isDiscrete = error "Not implemented"
+
+isDiscreteFn :: (Double -> Double) -> Bool
+isDiscreteFn = error "Not implemented"
+
+isPMF :: PMF a -> Bool
+isPMF (StrictPMF (RandomVariable ps rv)) = isDiscrete (events ps)
+isPMF (ArbitraryPMF pmf) = -- Shortcut when the RV isn't known.
+    isDiscreteFn pmf && (forAll (asList Reals) $ \x -> pmf x >= 0 && x <= 1.0)
+    && (sum $ map pmf (asList Reals)) == 1.0
