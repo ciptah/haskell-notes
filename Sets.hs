91705eb57ed9bbd9ed743c531a4bc95ddb2596e4
cipta <ciphwn@gmail.com>
2018-05-24 20:32:37 -0400

    more defined

--- Sets.hs
+++ Sets.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE ExistentialQuantification #-}
--- Set theory formalized in Haskell.
+-- Set theory in Haskell.
+-- There are two "primitive" operations:
+--   1. Checking existence of an element in the set satisfying a predicate
+--   2. Pulling out the sole element of a singleton set.
+-- The rest can be defined using these two without any circular definitions.
 
 module Sets (
   Set(Everything, Subset),
@@ -7,26 +10,25 @@ module Sets (
   (%),
   Collection,
   RealNum,
-  reals, r1, integers, r2, nonnegatives, naturals,
   member, notMember, (∈), (∉), -- u2208, u2209
   thereExists', forAll', eval, prove, thereExists, forAll,
-  intersect, union, minus, complement
-  -- isEmpty,
+  intersect, union, minus, complement, (∪), (∩), -- u222a, u2229
+  unionAll, intersectAll,
+  image, preimage,
   -- isSubsetOf,
-  -- forAll,
-  -- unionAll,
-  -- asList,
-  -- fromList,
   -- isDisjoint,
   -- isAllDisjoint,
   -- star,
   -- countableUnions,
-  -- singleton,
+  isSingleton, singleton,
+  reals, r1, integers, r2, nonnegatives, naturals,
+  empty -- The empty set.
 ) where
 
 import Data.Maybe (Maybe)
 
 -- A (mathematical) set of x.
+-- Just by these definitions it's possible to 
 data Set w = Everything | Subset (w -> Bool) (Set w)
 
 -- A statement about some element with a certain property existing in the set.
@@ -40,17 +42,6 @@ set % predicate = Subset predicate set
 -- Not "completely" true but it's OK for now.
 type Collection = Set
 type RealNum = Double
-
-
-
--- Some examples.
-reals = Everything :: Set RealNum
-r1 = reals
-integers = Everything :: Set Integer
-r2 = Everything :: Set (RealNum, RealNum)
-nonnegatives = integers % \i -> i >= 0 -- non-negative integers.
-naturals = integers % \i -> i > 0 -- natural numbers
-empty = Everything % \x -> False -- empty set.
 
 
 
@@ -81,11 +72,11 @@ neg (Not x) = x
 
 eval :: Statement a -> Bool
 eval (Not x) = not $ eval x
-eval (Exists inSet predicate) = error "Uncomputable!!"
+eval (Exists inSet predicate) = error "** Magic **"
 
 -- Exists statements can be proven, if you supply a satisfying element
 prove (Exists inSet predicate) proof =
-    if proof ∈ inSet && predicate proof then "QED" else error "Doh!!"
+    if proof ∈ inSet && predicate proof then "QED" else error "D'oh!!"
 
 thereExists inSet predicate = eval $ thereExists' inSet predicate
 forAll inSet predicate = eval $ forAll' inSet predicate
@@ -102,68 +93,53 @@ infixl 7 ∩ -- u2229
 (∪) = union
 (∩) = intersect
 
-unionAll = (Foldable t) => t (Set a) -> Set a
-unionAll = foldr (∪) empty
+unionAll :: (Foldable t) => t (Set a) -> Set a
+unionAll = foldr (∪) empty -- Currying
+intersectAll :: (Foldable t) => t (Set a) -> Set a
+intersectAll = foldr (∩) Everything
+
+-- Equality using Axiom of extensionality from ZFC
+-- Because we use forAll in the definition of Eq, redefining thereExists
+--   as (set % condition != empty) will cause a circular definition.
+-- We have to make either "there exists" or "Eq" an undefinable primitive.
+instance Eq (Set a) where
+  x == y = forAll Everything $ \z -> (z ∈ x && z ∈ y) || (z ∉ x && z ∉ y)
+
+-- Find the preimage of a set in the range under the function.
+preimage :: (a -> b) -> Set b -> Set a
+preimage fn image = Everything % \x -> fn x `member` image
+
+-- I don't think it's possible to define this without using Eq or adding
+-- some other constructor.
+-- The image function is almost like fmap but only when b is Equalizable.
+-- The (Eq b) is what's preventing Set from becoming an instance of Functor.
+image :: (Eq b) => (a -> b) -> Set a -> Set b
+image fn domain =
+    Everything % \y -> thereExists domain $ \x -> (fn x) == y
+
+-- Cartesian product.
+cartesian :: Set a -> Set b -> Set (a, b)
+cartesian setA setB = Everything % \(x, y) -> x ∈ setA && y ∈ setB
+
+-- Singleton means there are two things in the set that aren't the same.
+isSingleton :: (Eq a) => Set a -> Bool
+isSingleton set =
+  set /= empty && (not $ thereExists (cartesian set set) $ \(x, y) -> x /= y)
+
+-- Pulling things out of a singleton set can't be done with just a
+-- "there exists" oracle.
+singleton :: (Eq a) => Set a -> Maybe a
+singleton set | set == empty = Nothing
+              | isSingleton set = error "** Magic **"
+              | otherwise = Nothing
+
+-- isEmpty isn't necessary. Just compare with empty
+isSubsetOf :: Set a -> Set a -> Bool
+a `isSubsetOf` b = a == (a ∩ b)
 
 --
 --
--- -- Mathematical "For all"
--- forAll :: (Foldable t) => t a -> (a -> Bool) -> Bool
--- forAll s pred = foldr (\x z -> (pred x) && z) True s
 --
--- -- Mathematical "There Exists"
--- thereExists :: (Foldable t) => t a -> (a -> Bool) -> Bool
--- thereExists s pred = foldr (\x z -> (pred x) || z) False s
---
--- instance Show (Set a) where
---   show EmptySet = "{}"
---   show x = error "Not Implemented"
---
--- instance Eq (Set a) where
---   (==) EmptySet EmptySet = True
---   (==) x y = error "Not Implemented"
---
--- instance Foldable Set where
---   foldr f z EmptySet = z
---   foldr f z s = error "Not implemented"
---
--- instance Functor Set where
---   fmap fn EmptySet = EmptySet
---   fmap fn set = error "Not implemented"
---
--- -- Declare all the common set operations.
---
--- isEmpty :: Set a -> Bool
--- isEmpty EmptySet = True
--- isEmpty _ = False
---
---
--- isSubsetOf :: Set a -> Set a -> Bool
--- isSubsetOf EmptySet _ = True
--- isSubsetOf _ _ = error "Not implemented"
---
--- minus :: (Eq a) => Set a -> Set a -> Set a
--- minus EmptySet _ = EmptySet
--- minus x EmptySet = x
--- minus x y | x == y = EmptySet
--- minus _ _ = error "Not implemented"
---
--- union :: Set a -> Set a -> Set a
--- union = error "Not implemented"
---
--- intersect :: Set a -> Set a -> Set a
--- intersect = error "Not implemented"
---
--- unionAll :: [Set a] -> Set a
--- unionAll sets = foldr (union) EmptySet sets
---
--- asList :: Set a -> [a]
--- asList EmptySet = []
--- asList _ = error "Not implemented"
---
--- fromList :: [a] -> Set a
--- fromList [] = EmptySet
--- fromList _ = error "Not implemented"
 --
 -- -- Disjoint, aka no intersection.
 -- isDisjoint :: (Eq a) => Set a -> Set a -> Bool
@@ -186,8 +162,14 @@ unionAll = foldr (∪) empty
 -- -- unions of the elements in the set.
 -- countableUnions :: Collection (Set a) -> Set (Set a)
 -- countableUnions sets = fmap unionAll $ star sets
---
--- -- Unpack: If singleton, output inner value. Otherwise, None.
--- singleton :: Set a -> Maybe a
--- singleton (Singleton x) = Just x
--- singleton _ = Nothing
+
+
+
+-- Some examples.
+reals = Everything :: Set RealNum
+r1 = reals
+integers = Everything :: Set Integer
+r2 = Everything :: Set (RealNum, RealNum)
+nonnegatives = integers % \i -> i >= 0 -- non-negative integers.
+naturals = integers % \i -> i > 0 -- natural numbers
+empty = Everything `minus` Everything
